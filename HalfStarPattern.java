efd622554d8a8c16ea30dd0d0135e86194a9d40a
Arbab Ali <93856092+ArbabAli119@users.noreply.github.com>
2026-07-18 01:34:01 +0530

    Update HalfStarPattern.java

--- HalfStarPattern.java
+++ HalfStarPattern.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HalfStarPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter The Number :");
+        System.out.print("Enter The Number : ");
         int n = sc.nextInt();
 
         for (int line=1; line <=n; line++) {
